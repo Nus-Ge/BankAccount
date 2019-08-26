205e969867113c96d7174361ad85e163be3541a5
Nus-Ge <54533327+Nus-Ge@users.noreply.github.com>
2019-08-26 21:24:33 +0800

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class BankAccount
+    {
+        protected string acctNumber;
+        protected string acctHolderId;
+        protected double balance;
+
+        public string AcctNumber
+        {
+            set
+            {
+                acctNumber = value;
+            }
+            get
+            {
+                return acctNumber;
+            }
+        }
+
+        public string AcctHolderId
+        {
+            set
+            {
+                acctHolderId = value;
+            }
+            get
+            {
+                return acctHolderId;
+            }
+        }
+
+        public double Balance
+        {
+            set
+            {
+                balance = value;
+            }
+            get
+            {
+                return balance;
+            }
+        }
+
+        public BankAccount(string a, string b, double c)
+        {
+            acctNumber = a;
+            acctHolderId = b;
+            balance = c;
+        }
+        public void Deposit(double amt)
+        {
+            Balance = Balance + amt;
+        }
+
+        public void TansferTo(double amt, BankAccount another)
+        {
+            Balance -= amt;
+            another.Balance += amt;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account_me = new BankAccount("12345","gao_ge",1234);
+            BankAccount account_JPMorgan = new BankAccount("123456", "JPMorgan", 6666);
+            account_me.TansferTo(1000, account_JPMorgan);
+            Console.WriteLine(account_me.Balance);
+        }
+    }
+}
